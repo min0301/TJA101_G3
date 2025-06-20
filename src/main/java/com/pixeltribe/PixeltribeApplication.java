5dc2bf49ccbd373efb8d1ea794382bcb1bcfb3c4
TommyHCY <chienyu749@gmail.com>
2025-06-20 20:48:55 +0800

    init project

---
+++ src/main/java/com/pixeltribe/PixeltribeApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixeltribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixeltribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixeltribe/PixeltribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixeltribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixeltribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
