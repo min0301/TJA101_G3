27c656c0584323d44fe1dea965a453fcc5ffad46
jimmy <min0301.tw@gmail.com>
2025-06-27 19:06:31 +0800

    使用ＡＰＩ方式連結給大家參考

---
+++ src/main/java/com/pixeltribe/forumsys/forum/controller/ForumController.java
@@ -0,0 +1,50 @@
+package com.pixeltribe.forumsys.forum.controller;
+
+import com.pixeltribe.forumsys.forum.model.Forum;
+import com.pixeltribe.forumsys.forum.model.ForumService;
+import com.pixeltribe.forumsys.forumcategory.model.ForumCategoryService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ForumController {
+
+    @Autowired
+    ForumService forumSvc;
+
+    @Autowired
+    ForumCategoryService forumCategorySvc;
+
+
+//    @GetMapping("/listAllForum")
+//    public String listAllForums(Model model) { // 1. 傳入 Model 物件
+//
+//        // 2. 直接在此方法內查詢資料
+//        // 注意：請確認你的 Service 方法名是 getAllForum() 還是 getAllForums()，要完全一致
+//        List<Forum> list = forumSvc.getAllForum();
+//
+//        // 3. 將查到的資料放入 Model
+//        model.addAttribute("forums", list);
+//
+//        // 4. 返回視圖名稱
+//        return "back-end/forum/listAllForum";
+//    }
+
+    @GetMapping("AllForum")
+    public List<Forum> findAll() {
+
+        return forumSvc.getAllForum();
+
+    }
+
+
+}
