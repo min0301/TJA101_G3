ddda833a191136a9e5f226444dac76bedfb9ac77
TommyHCY <chienyu749@gmail.com>
2025-06-30 16:06:06 +0800

    remove duplicate

--- src/main/java/com/pixeltribe/shopsys/mallTag/model/MallTagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pixeltribe.shopsys.malltag.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MallTagRepository extends JpaRepository<MallTag, Integer>{
-
-}
