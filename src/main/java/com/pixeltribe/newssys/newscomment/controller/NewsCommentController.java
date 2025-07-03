ef13c63ed6ce41c477a879de3a028144a12f10e2
TommyHCY <chienyu749@gmail.com>
2025-07-03 19:08:26 +0800

    新增 News.html NewsCommentController 新增 新聞評論的API 修改 NewsHomePage.html 可以連接到 News.html

--- src/main/java/com/pixeltribe/newssys/newscomment/controller/NewsCommentController.java
+++ src/main/java/com/pixeltribe/newssys/newscomment/controller/NewsCommentController.java
@@ -1,9 +1,11 @@
 package com.pixeltribe.newssys.newscomment.controller;
 
 import com.pixeltribe.newssys.newscomment.model.NewsComment;
+import com.pixeltribe.newssys.newscomment.model.NewsCommentDTO;
 import com.pixeltribe.newssys.newscomment.model.NewsCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,8 +18,8 @@ class NewsCommentController {
     @Autowired
     NewsCommentService newsCommentService;
 
-    @GetMapping("NewsComment")
-    public List<NewsComment> findAll(){
-        return newsCommentService.findAll();
+    @GetMapping("NewsComment/{id}")
+    public List<NewsCommentDTO> findAll(@PathVariable Integer id){
+        return newsCommentService.findAll(id);
     }
 }
---
+++ src/main/java/com/pixeltribe/newssys/newscomment/model/NewsCommentDTO.java
@@ -0,0 +1,29 @@
+package com.pixeltribe.newssys.newscomment.model;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Value;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * DTO for {@link NewsComment}
+ */
+@Value
+public class NewsCommentDTO implements Serializable {
+    Integer id;
+    @NotNull
+    @Size(max = 4000)
+    String ncomCon;
+    @NotNull
+    Instant ncomCre;
+    @NotNull
+    Character ncomStatus;
+    Integer memNoMemNo;
+    String memNoMemNickName;
+    @NotNull
+    Integer ncomLikeLc;
+    @NotNull
+    Integer ncomLikeDlc;
+}
