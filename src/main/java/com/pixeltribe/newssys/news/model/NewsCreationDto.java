da83adec83a65e0319b5e916fa0b5ece6e792424
TommyHCY <chienyu749@gmail.com>
2025-07-03 12:12:51 +0800

    排版index 增加newshomepage.html

--- src/main/java/com/pixeltribe/newssys/news/model/NewsCreationDto.java
+++ src/main/java/com/pixeltribe/newssys/news/model/NewsCreationDto.java
@@ -5,16 +5,24 @@ import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * DTO for {@link News}
  */
 @Value
-public class CreateNewsDto implements Serializable {
+public class NewsCreationDto implements Serializable {
     @NotNull
     @Size(max = 255)
     String newsTit;
     @NotNull
     @Size(max = 9000)
     String newsCon;
+
+    Integer imgCount;
+
+    @NotNull(message = "至少要有一個分類")
+    List<Integer> tags;
+
+
 }
