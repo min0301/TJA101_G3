fe8e211e85883d1a5ad42ef035b0a603dc8166ba
akuei <akueiyh@gmail.com>
2025-07-09 16:33:31 +0800

    新增會員註冊功能(初版，使用體驗待優化)

--- src/main/java/com/pixeltribe/membersys/member/model/MemRepository.java
+++ src/main/java/com/pixeltribe/membersys/member/model/MemRepository.java
@@ -7,4 +7,5 @@ public interface MemRepository extends JpaRepository<Member, Integer> {
 	Member findByMemAccount (String memAccount);
 	Member findByMemPassword(String memPassword);
 	Member findByMemEmail(String memEmail);
+	boolean existsByMemEmail(String memEmail);
 }
