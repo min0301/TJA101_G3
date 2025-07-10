b6f8b940975eae7ea5f07421a6fce48d560b5f34
jimmy <min0301.tw@gmail.com>
2025-07-10 10:44:50 +0800

    debug:  討論區收藏

--- src/main/java/com/pixeltribe/forumsys/forumcollect/controller/ForumCollectController.java
+++ src/main/java/com/pixeltribe/forumsys/forumcollect/controller/ForumCollectController.java
@@ -7,11 +7,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class ForumCollectController {
 
     private final ForumCollectService forumCollectService;
