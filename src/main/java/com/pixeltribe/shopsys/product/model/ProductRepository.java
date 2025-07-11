eb617131607bd33551f2eb10639c2b2f88390985
littleight <littleight@localhost>
2025-07-11 12:31:56 +0800

    更新ProductRepository

--- src/main/java/com/pixeltribe/shopsys/product/model/ProductRepository.java
+++ src/main/java/com/pixeltribe/shopsys/product/model/ProductRepository.java
@@ -10,10 +10,10 @@ import org.springframework.data.repository.query.Param;
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 	
 		@Modifying
-	    @Query("UPDATE Product p SET p.PRO_ISMARKET = :proIsMarket WHERE p.id = :proNo")
+	    @Query(value="UPDATE Product p SET p.PRO_ISMARKET = :proIsMarket WHERE p.id = :proNo", nativeQuery = true)
 	    Integer updateMarketStatus(@Param("proNo") Integer proNo, @Param("proIsmarket") Character proIsMarket);
 		
-	    @Query("SELECT * FROM Product p WHERE p.MALL_TAG_NO = :malltagNo "+"AND (p.PRO_ISMARKET IS NULL OR p.PRO_ISMARKET = :proIsMarket)")
+	    @Query(value="SELECT * FROM Product p WHERE p.MALL_TAG_NO = :malltagNo "+"AND (p.PRO_ISMARKET IS NULL OR p.PRO_ISMARKET = :proIsMarket)", nativeQuery = true)
 	    List<ProductSearchDTO> findByMallTagWithMarket(@Param("malltagNo") Integer malltagNo,@Param("proIsmarket") Character proIsMarket);
 
 }
