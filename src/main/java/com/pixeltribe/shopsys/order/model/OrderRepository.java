9c219051bd27ef31ba85e87b5978249173d79645
Joanna Lin <joanna.yu0820@gmail.com>
2025-07-13 00:56:24 +0800

    訂單-結帳功能修正

--- src/main/java/com/pixeltribe/shopsys/order/model/OrderRepository.java
+++ src/main/java/com/pixeltribe/shopsys/order/model/OrderRepository.java
@@ -3,24 +3,27 @@ package com.pixeltribe.shopsys.order.model;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 	
-	// 按照訂單時間降序排列
-	List<Order> findByMemNo_MemNoOrderByOrderDatetimeDesc(Integer memNo);
-	
-	// 根據會員編號查詢訂單，按照訂單編號降序排列
-    // 因為 Order.memNo 是Member物件，所以要用 memNo_memNo
-    List<Order> findByMemNo_MemNoOrderByOrderNoDesc(Integer memNo);
-   
-	
-	
+
 	// 跟訂單查詢狀態查詢
 	List<Order> findByOrderStatus(String Status);
-	
 	Order findByOrderNo(Integer orderNo);
+	
+	// 根據會員編號查詢訂單，按時間降序排列
+    @Query(value = "SELECT * FROM order_table o WHERE o.MEM_NO = :memNo ORDER BY o.ORDER_DATETIME DESC", nativeQuery = true)
+    List<Order> findByMemNoOrderByOrderDatetimeDesc(@Param("memNo") Integer memNo);
+    
+    // 根據會員編號查詢訂單，按訂單編號降序排列
+    @Query(value = "SELECT * FROM order_table o WHERE o.MEM_NO = :memNo ORDER BY o.ORDER_NO DESC", nativeQuery = true)
+    List<Order> findByMemNoOrderByOrderNoDesc(@Param("memNo") Integer memNo);
+	
+	
 
 	
 }
